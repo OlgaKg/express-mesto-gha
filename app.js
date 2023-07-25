4b5fe521be11ba837741931ca3faf6c47b7e5bd1
OlgaKG <flory_oli@mail.ru>
2023-07-25 10:12:12 +0200

    13PR

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ app.use(express.json());
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   req.user = {
     _id: '64bc68f10e16c7c7b9f31183',
   };
   next();
 });
 
-app.use(routes);
+app.use('/', routes);
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
