65d21031f667cf1482223e9debe65115dd6d3801
OlgaKG <flory_oli@mail.ru>
2023-07-27 01:40:10 +0200

    13PR/app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const routes = require('./routes/index');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '64bc68f10e16c7c7b9f31183',
-  };
-  next();
-});
+// app.use((req, res, next) => {
+//   req.user = {
+//     _id: '64bc68f10e16c7c7b9f31183',
+//   };
+//   next();
+// });
 
 app.use(express.json());
 app.use(routes);
